3e2a5cdc1fedc1e483dfee3ad42c859c802496b3
Yussuf Olaoluwa <yussufolaoluwa2@gmail.com>
2026-07-13 03:16:50 +0100

    Test Hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
+import Hero from "../components/Hero";
+
 export default function Home() {
   return (
-    <div
-      style={{
-        color: "white",
-        textAlign: "center",
-        padding: "60px",
-      }}
-    >
-      <h1>Home Page Works ✅</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
