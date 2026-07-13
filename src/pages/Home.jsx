3a18a7a9fade112609dba019aba36f9787be333f
Yussuf Olaoluwa <yussufolaoluwa2@gmail.com>
2026-07-13 03:25:44 +0100

    Test CategoryFilter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import Hero from "../components/Hero";
 import FlashSale from "../components/FlashSale";
 import Categories from "../components/Categories";
 import SearchBar from "../components/SearchBar";
+import CategoryFilter from "../components/CategoryFilter";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("All");
 
   return (
     <>
@@ -13,6 +15,10 @@ export default function Home() {
       <FlashSale />
       <Categories />
       <SearchBar onSearch={setSearchTerm} />
+      <CategoryFilter
+        selectedCategory={category}
+        onCategoryChange={setCategory}
+      />
     </>
   );
 }
