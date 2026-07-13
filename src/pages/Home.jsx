53608b9ba6ce67c55fba631481f3cf86b048d997
Yussuf Olaoluwa <yussufolaoluwa2@gmail.com>
2026-07-13 03:23:08 +0100

    Test SearchBar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import Hero from "../components/Hero";
 import FlashSale from "../components/FlashSale";
 import Categories from "../components/Categories";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <>
       <Hero />
       <FlashSale />
       <Categories />
+      <SearchBar onSearch={setSearchTerm} />
     </>
   );
 }
