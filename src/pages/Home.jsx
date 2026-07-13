ac95dbd947720453eac461a6bf4520787cd998ed
Yussuf Olaoluwa <yussufolaoluwa2@gmail.com>
2026-07-13 03:21:14 +0100

    Test Categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero";
 import FlashSale from "../components/FlashSale";
+import Categories from "../components/Categories";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FlashSale />
+      <Categories />
     </>
   );
 }
