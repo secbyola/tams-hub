1837f93a49041b74d7b9548cc4a1e1c2cdc376a8
Yussuf Olaoluwa <yussufolaoluwa2@gmail.com>
2026-07-16 16:02:56 +0100

    Fix Context imports

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-import { ThemeProvider } from "./context/ThemeContext";
-import { CartProvider } from "./context/CartContext";
-import { WishlistProvider } from "./context/WishlistContext";
-
+import { ThemeProvider } from "./Context/ThemeContext";
+import { CartProvider } from "./Context/CartContext";
+import { WishlistProvider } from "./Context/WishlistContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
